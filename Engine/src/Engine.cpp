8c644d6ae99330f65bedfe3caaa167e52621bb6e
Matt Smith <S3769858@student.rmit.edu.au>
2022-12-10 23:46:53 +1100

    Engine set as an object
    Engine set as a class object to split out member variables and functions into private and public sections as initial GLFW and GLAD loading order is critical and needs to be controlled. Also set up TODOs for creating vertex array objects, vertex buffers, index buffers and shaders.

--- Engine/src/Engine.cpp
+++ Engine/src/Engine.cpp
@@ -1,7 +1,90 @@
 #include "Engine.h"
 #include <iostream>
 
-void testPrint()
+Engine::Engine()
+{
+	initGLFW();
+	initGLAD();
+}
+
+Engine::~Engine()
+{
+
+}
+
+void Engine::framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+bool Engine::initGLFW()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWmonitor* monitorWindow = NULL;
+    if (!m_windowedMode)
+    {
+        monitorWindow = glfwGetPrimaryMonitor();
+        const GLFWvidmode* monitorMode = glfwGetVideoMode(monitorWindow);
+        m_SCREEN_WIDTH = monitorMode->width;
+        m_SCREEN_HEIGHT = monitorMode->height;
+    }
+
+    // Initialise a window, throw an error if window failed to initialise
+    m_window = glfwCreateWindow(m_SCREEN_WIDTH, m_SCREEN_HEIGHT, "Window Title", monitorWindow, NULL);
+    if (m_window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return false;
+    }
+    glfwMakeContextCurrent(m_window);
+
+    // Set what function to call when the window is resized, passed as a parameter
+    glfwSetFramebufferSizeCallback(m_window, framebuffer_size_callback);
+
+    // Set framerate
+    glfwSwapInterval(10);
+
+    return true;
+}
+
+bool Engine::initGLAD()
+{
+    // Initialise GLAD, throw an error if GLAD failed to initialise
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool Engine::createVertexArray()
+{
+    return false;
+}
+
+bool Engine::createVertexBuffer()
+{
+    return false;
+}
+
+bool Engine::createIndexBuffer()
+{
+    return false;
+}
+
+bool Engine::createShader()
+{
+    return false;
+}
+
+void Engine::testPrint()
 {
 	std::cout << "Hello World" << std::endl;
 }
--- Engine/src/Engine.h
+++ Engine/src/Engine.h
@@ -1,6 +1,51 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-void testPrint();
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Engine
+{
+	// Screen sizes, default values set at 800 x 600
+	unsigned int m_SCREEN_WIDTH = 800;
+	unsigned int m_SCREEN_HEIGHT = 600;
+
+	bool m_windowedMode = true;
+
+	GLFWwindow* m_window;
+
+	// Initialize GLFW for windowing
+	bool initGLFW();
+
+	// Sets glViewport with given width and height. Used as a function to be called on resize
+	static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+	// Initialize GLAD for OpenGL calls
+	bool initGLAD();
+
+public:
+
+	void testPrint();
+
+	// Engine constructor. Will initialize a main GLFW window and load GLAD to be used for OpenGL calls.
+	// Important for inital GLFW and GLAD load order to be controlled otherwise GLAD will not load correctly.
+	Engine();
+	~Engine();
+
+	// Initialize Vertex Array Object
+	bool createVertexArray();
+
+	// Initialize Vertex Buffer
+	bool createVertexBuffer();
+
+	// Initialize Index Buffer
+	bool createIndexBuffer();
+
+	// Initialize Shader
+	bool createShader();
+
+};
 
 #endif // !ENGINE_H
