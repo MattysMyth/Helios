c6e10d8dbd10cc532c5ed217db0f41d77ddba0ad
Matt Smith <S3769858@student.rmit.edu.au>
2022-12-17 14:38:25 +1100

    Moved Shader files to Shader directory

---
+++ Engine/src/Shader/Shader.cpp
@@ -0,0 +1,109 @@
+#include "Shader.h"
+
+Shader::Shader()
+{
+
+}
+
+Shader::~Shader()
+{
+
+}
+
+void Shader::createShader(const std::string& filePath, ShaderType shaderType)
+{
+    m_Type = shaderType;
+    m_SourceString = parseShader(filePath);
+    m_SourceCode = m_SourceString.c_str();
+
+    switch (m_Type)
+    {
+    case ShaderType::VERTEX:
+        m_ShaderID = glCreateShader(GL_VERTEX_SHADER);
+        break;
+    case ShaderType::FRAGMENT:
+        m_ShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+        break;
+    default:
+        break;
+    }
+
+    glShaderSource(m_ShaderID, 1, &m_SourceCode, nullptr);
+    glCompileShader(m_ShaderID);
+
+    // check for shader compile errors
+    int success;
+    glGetShaderiv(m_ShaderID, GL_COMPILE_STATUS, &success);
+    if (success == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(m_ShaderID, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(m_ShaderID, length, &length, message);
+        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << message << std::endl;
+    }
+}
+
+std::string Shader::parseShader(const std::string& filePath)
+{
+    std::ifstream stream(filePath);
+
+    std::stringstream ss;
+    std::string line;
+    while (std::getline(stream, line))
+    {
+        ss << line << '\n';
+    }
+
+    return ss.str();
+}
+
+unsigned int Shader::getID()
+{
+    return m_ShaderID;
+}
+
+void Shader::bind()
+{
+    glUseProgram(m_ShaderID);
+}
+
+void Shader::unbind()
+{
+    glUseProgram(0);
+}
+
+void Shader::uploadUniformInt(const std::string& name, int value)
+{
+
+}
+
+void Shader::uploadUniformFloat(const std::string& name, float value)
+{
+
+}
+
+void Shader::uploadUniformFloat2(const std::string& name, const glm::vec2& value)
+{
+
+}
+
+void Shader::uploadUniformFloat3(const std::string& name, const glm::vec3& value)
+{
+
+}
+
+void Shader::uploadUniformFloat4(const std::string& name, const glm::vec4& value)
+{
+
+}
+
+void Shader::uploadUniformMat3(const std::string& name, const glm::mat3& matrix)
+{
+
+}
+
+void Shader::uploadUniformMat4(const std::string& name, const glm::mat4& matrix)
+{
+
+}
---
+++ Engine/src/Shader/Shader.h
@@ -0,0 +1,53 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+enum class ShaderType
+{
+	VERTEX,
+	FRAGMENT
+};
+
+// Shader object that translates and stores shader code from external files into GPU friendly strings. Essentially
+// a CPU image of a Shader before being linked to a Program on the GPU
+class Shader
+{
+	ShaderType m_Type;
+
+	std::string m_SourceString;
+	const char* m_SourceCode;
+
+	unsigned int m_ShaderID;
+
+	std::string parseShader(const std::string& filePath);
+
+public:
+
+	Shader();
+	~Shader();
+
+	void createShader(const std::string& filePath, ShaderType shaderType);
+
+	unsigned int getID();
+
+	void bind();
+
+	void unbind();
+
+	void uploadUniformInt(const std::string& name, int value);
+
+	void uploadUniformFloat(const std::string& name, float value);
+	void uploadUniformFloat2(const std::string& name, const glm::vec2& value);
+	void uploadUniformFloat3(const std::string& name, const glm::vec3& value);
+	void uploadUniformFloat4(const std::string& name, const glm::vec4& value);
+
+	void uploadUniformMat3(const std::string& name, const glm::mat3& matrix);
+	void uploadUniformMat4(const std::string& name, const glm::mat4& matrix);
+};
+
+#endif // !SHADER_H
