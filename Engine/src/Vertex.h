8fab68b48bd1fdfc8e495f91b9b506fb49a0d30f
MattysMyth <matthew.smith2193@gmail.com>
2022-12-18 15:27:40 +1100

    Update Vertex.h
    Updated Vertex structure to include Position Structs, Color structs and a set of constructors

--- Engine/src/Vertex.h
+++ Engine/src/Vertex.h
@@ -1,17 +1,43 @@
 #ifndef VERTEX_H
 #define VERTEX_H
 
-class Vertex
+struct Position
 {
-	struct Position
-	{
-		float x, y, z;
-	};
-
-	struct Color
-	{
-		float red, green, blue, alpha;
-	};
+	float x, y, z;
 };
+
+struct Color
+{
+	float red, green, blue, alpha;
+};
+
+struct Vertex
+{
+
+	Position position;
+	Color color;
+
+	Vertex();
+	Vertex(Position position, Color color);
+
+};
+
+Vertex::Vertex()
+{
+	position.x = 0.0f;
+	position.y = 0.0f;
+	position.z = 0.0f;
+
+	color.red = 1.0f;
+	color.green = 1.0f;
+	color.blue = 1.0f;
+	color.alpha = 1.0f;
+}
+
+Vertex::Vertex(Position position, Color color)
+{
+	this->position = position;
+	this->color = color;
+}
 
 #endif // !VERTEX_H
