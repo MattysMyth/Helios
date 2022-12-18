cbb1395350a04b1877dd7f31e62ef850ff8cd335
Matt Smith <S3769858@student.rmit.edu.au>
2022-12-18 13:25:15 +1100

    Create Vertex.h
    Vertex object to hold all vertex data

---
+++ Engine/src/Vertex.h
@@ -0,0 +1,17 @@
+#ifndef VERTEX_H
+#define VERTEX_H
+
+class Vertex
+{
+	struct Position
+	{
+		float x, y, z;
+	};
+
+	struct Color
+	{
+		float red, green, blue, alpha;
+	};
+};
+
+#endif // !VERTEX_H
