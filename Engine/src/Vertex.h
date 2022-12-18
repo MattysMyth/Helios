945341c6b8d17252315fb3b31351ce81d680a07d
Matt Smith <S3769858@student.rmit.edu.au>
2022-12-18 21:50:45 +1100

    Update Vertex.h
    Removed Vertex.cpp implementation

--- Engine/src/Vertex.h
+++ Engine/src/Vertex.h
@@ -22,22 +22,4 @@ struct Vertex
 
 };
 
-Vertex::Vertex()
-{
-	position.x = 0.0f;
-	position.y = 0.0f;
-	position.z = 0.0f;
-
-	color.red = 1.0f;
-	color.green = 1.0f;
-	color.blue = 1.0f;
-	color.alpha = 1.0f;
-}
-
-Vertex::Vertex(Position position, Color color)
-{
-	this->position = position;
-	this->color = color;
-}
-
 #endif // !VERTEX_H
