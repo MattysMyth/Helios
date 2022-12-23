8233a56254258ac4f39b4319b7d33ff143e931c3
MattysMyth <matthew.smith2193@gmail.com>
2022-12-23 20:02:48 +1100

    Update Launch.cpp
    Added shape and calls to Engine to load the shape model and then call the engine render loop

--- Application/src/Launch.cpp
+++ Application/src/Launch.cpp
@@ -1,8 +1,14 @@
 #include <Engine.h>
+#include "Shape.h"
 
 int main()
 {
 	Engine* engine = new Engine();
+	Shape* shape = new Shape();
+
+	engine->loadModel(shape);
+
+	engine->render();
 	
 	return 0;
 }
