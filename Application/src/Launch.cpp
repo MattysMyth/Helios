7839c325321ab4bc21d674d6ed99368a3ba27f0e
MattysMyth <matthew.smith2193@gmail.com>
2022-12-26 12:57:18 +1100

    Update Launch.cpp
    Split out shape into two separate triangle objects to form a rectangle. This is to help test multiple objects being uploaded to the Engine and then GPU and being rendered correctly.

--- Application/src/Launch.cpp
+++ Application/src/Launch.cpp
@@ -1,12 +1,19 @@
 #include <Engine.h>
-#include "Shape.h"
+#include "Triangle1.h"
+#include "Triangle2.h"
 
 int main()
 {
 	Engine* engine = new Engine();
-	Shape* shape = new Shape();
+	Triangle1* triangle1 = new Triangle1();
+	Triangle2* triangle2 = new Triangle2();
 
-	engine->loadModel(shape);
+	std::vector<Entity*> entities;
+
+	entities.push_back(triangle1);
+	entities.push_back(triangle2);
+
+	engine->loadEntities(entities);
 
 	engine->render();
 	
