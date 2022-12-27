1069401e564d821b29c05d66a77fb9a2c589a060
MattysMyth <matthew.smith2193@gmail.com>
2022-12-27 13:26:30 +1100

    Set Triangle colours
    Set one triangles vertices to white and the other to red for testing

--- Application/src/Triangle1.cpp
+++ Application/src/Triangle1.cpp
@@ -2,7 +2,7 @@
 
 Triangle1::Triangle1()
 {
-    Vertex vert1, vert2, vert3, vert4;
+    Vertex vert1, vert2, vert3;
 
     vert1.position.x = -0.5f;
     vert1.position.y = -0.5f;
@@ -13,21 +13,14 @@ Triangle1::Triangle1()
     vert3.position.x = 0.5f;
     vert3.position.y = 0.5f;
 
-    vert4.position.x = 0.5f;
-    vert4.position.y = -0.5f;
-
     verticies.push_back(vert1);
     verticies.push_back(vert2);
     verticies.push_back(vert3);
-    verticies.push_back(vert4);
 
     indices.push_back(0);
     indices.push_back(1);
     indices.push_back(2);
 
-    indices.push_back(0);
-    indices.push_back(2);
-    indices.push_back(3);
 }
 
 Triangle1::~Triangle1()
--- Application/src/Triangle2.cpp
+++ Application/src/Triangle2.cpp
@@ -6,12 +6,24 @@ Triangle2::Triangle2()
 
     vert1.position.x = -0.5f;
     vert1.position.y = -0.5f;
+    vert1.color.red = 1.0f;
+    vert1.color.green = 0.0f;
+    vert1.color.blue = 0.0f;
+    vert1.color.alpha = 1.0f;
 
     vert2.position.x = 0.5f;
     vert2.position.y = 0.5f;
+    vert2.color.red = 1.0f;
+    vert2.color.green = 0.0f;
+    vert2.color.blue = 0.0f;
+    vert2.color.alpha = 1.0f;
 
     vert3.position.x = 0.5f;
     vert3.position.y = -0.5f;
+    vert3.color.red = 1.0f;
+    vert3.color.green = 0.0f;
+    vert3.color.blue = 0.0f;
+    vert3.color.alpha = 1.0f;
 
     verticies.push_back(vert1);
     verticies.push_back(vert2);
