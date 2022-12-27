4614e08eba05459bc3216a60f7b5696e306d22b3
MattysMyth <matthew.smith2193@gmail.com>
2022-12-27 13:29:40 +1100

    Update VBO.cpp
    Added Index abstraction for the position and colour Attributes. Also split out the code to Set Vertex Attributes into a separate helper function to be called when necessary

--- Engine/src/Renderer/VBO.cpp
+++ Engine/src/Renderer/VBO.cpp
@@ -1,5 +1,7 @@
 #include "VBO.h"
 
+#include <iostream>
+
 VBO::VBO()
 {
     glGenBuffers(1, &id);
@@ -11,15 +13,7 @@ VBO::VBO()
         GL_STATIC_DRAW
     );
 
-    glEnableVertexAttribArray(index);
-    glVertexAttribPointer(
-        index,
-        3, // Corresponding to x, y and z values
-        GL_FLOAT,
-        GL_FALSE,
-        sizeof(float) * 3, // 3 being the size of each set of x, y and z values
-        0
-    );
+    setVertexAttributes();
 }
 
 VBO::VBO(std::vector<Vertex> verticies)
@@ -42,15 +36,7 @@ VBO::VBO(std::vector<Vertex> verticies)
         GL_STATIC_DRAW
     );
 
-    glEnableVertexAttribArray(index);
-    glVertexAttribPointer(
-        index,
-        3, // Corresponding to x, y and z values
-        GL_FLOAT,
-        GL_FALSE,
-        sizeof(float) * 3, // 3 being the size of each set of x, y and z values
-        0
-    );
+    setVertexAttributes();
 }
 
 VBO::~VBO()
@@ -60,21 +46,14 @@ VBO::~VBO()
 
 void VBO::update(std::vector<Vertex> verticies)
 {
-    std::vector<float> positions;
-
-    for (size_t i = 0; i < verticies.size(); i++)
-    {
-        positions.push_back(verticies[i].position.x);
-        positions.push_back(verticies[i].position.y);
-        positions.push_back(verticies[i].position.z);
-    }
-
     glBufferData(
         GL_ARRAY_BUFFER,
-        sizeof(float) * positions.size(),
-        static_cast<void*>(positions.data()),
+        sizeof(Vertex) * verticies.size(),
+        &verticies.front(),
         GL_STATIC_DRAW
     );
+
+    setVertexAttributes();
 }
 
 void VBO::bind()
@@ -85,6 +64,31 @@ void VBO::bind()
 void VBO::unbind()
 {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void VBO::setVertexAttributes()
+{
+    // Position attribute
+    glEnableVertexAttribArray(positionIndex);
+    glVertexAttribPointer(
+        positionIndex,
+        3, // Corresponding to x, y and z values
+        GL_FLOAT,
+        GL_FALSE,
+        sizeof(float) * 7, // 3 being the size of each set of x, y and z values
+        0
+    );
+
+    // Color attribute
+    glEnableVertexAttribArray(colorIndex);
+    glVertexAttribPointer(
+        colorIndex,
+        4, // Corresponding to r, g, b and a values
+        GL_FLOAT,
+        GL_FALSE,
+        sizeof(float) * 7, // 4 being the size of each set of r, g, b and a values
+        (void*)(sizeof(float) * 3) // Offset by 3 floats. The order of bytes in the VBO is 3 position float followed by 4 color floats. 
+    );
 }
 
 unsigned int VBO::getID()
