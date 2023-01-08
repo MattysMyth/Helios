856f3514f66d0e9f536ad2590d2292a5ef7e857e
MattysMyth <matthew.smith2193@gmail.com>
2023-01-08 22:14:13 +1100

    Update Window.h
    Split the Input functions to be under the namespace Input and the Window functions and variables to be under the Window class. Both are included in the Helios namespace

--- Engine/Window.h
+++ Engine/Window.h
@@ -8,133 +8,141 @@
 #include "GLFW/glfw3.h"
 #include <iostream>
 
-// Object to handle GLFW instances and input functionality
-class Window
+namespace Helios
 {
-    using Callback = std::function<void()>;
+	// Object to handle GLFW instances and input functionality
+	class Window
+	{
+		using Callback = std::function<void()>;
 
-public:
-	Window();
-	~Window();
+	public:
+		Window();
+		~Window();
 
-	void defaultEscape();
+		/*
+		Pointer to an instance of a GLFW Window
+		*/
+		GLFWwindow* m_GLFWinstance;
 
-	/*
-	Adds a binding between a given functionand the key pressed. When pressed, GLFW will register the event and when polled will  
-	call the bound function
-	*/
-	void addKeyPressBinding(int key, const Callback callback);
+		/*
+		Map of all bound key press functions and associated keys
+		*/
+		std::map<int, Callback> m_KeyPressCallbacks;
 
-	/*
-	Adds a binding between a given functionand the key released. When released, GLFW will register the event and when polled will
-	call the bound function
-	*/
-	void addKeyReleaseBinding(int key, const Callback callback);
+		/*
+		Map of all bound key release functions and associated keys
+		*/
+		std::map<int, Callback> m_KeyReleaseCallbacks;
 
-	/*
-	Adds a binding between a given functionand the key held. When held, GLFW will register the event and when polled will
-	call the bound function
-	*/
-	void addKeyHoldBinding(int key, const Callback callback);
+		/*
+		Map of all bound key hold functions and associated keys
+		*/
+		std::map<int, Callback> m_KeyHoldCallbacks;
 
-	/*
-	Removes any key press function binding on the given key
-	*/
-	void removeKeyPressBinding(int key);
+	private:
+		/*
+		Window width, default value set to 800 pixels
+		*/
+		unsigned int m_SCREEN_WIDTH = 800;
 
-	/*
-	Removes any key release function binding on the given key
-	*/
-	void removeKeyReleaseBinding(int key);
+		/*
+		Window height, default value set to 600 pixels
+		*/
+		unsigned int m_SCREEN_HEIGHT = 600;
 
-	/*
-	Removes any key hold function binding on the given key
-	*/
-	void removeKeyHoldBinding(int key);
+		/*
+		Boolean value representing Fullscreen or windowed. If TRUE, application will run windowed
+		with the dimensions defined by m_SCREEN_WIDTH and m_SCREEN_HEIGHT. If FALSE, will
+		run full screen with monitor dimensions
+		*/
+		bool m_windowedMode = true;
 
-	/*
-	Swaps the key associated with a bound key press function
-	*/
-	void swapKeyPressBinding(int formerKey, int newKey);
+		/*
+		Function to be called on Window resize
+		*/
+		static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
-	/*
-	Swaps the key associated with a bound key release function
-	*/
-	void swapKeyReleaseBinding(int formerKey, int newKey);
+		/*
+		Function to be called on key event
+		*/
+		static void keyEventCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+	};
 
-	/*
-	Swaps the key associated with a bound key hold function
-	*/
-	void swapKeyHoldBinding(int formerKey, int newKey);
+	namespace Input
+	{
+		using Callback = std::function<void()>;
 
-	/*
-	Executes bound functions for pressed keys
-	*/
-	void onKeyPress(int key);
+		void defaultEscape(Window* window);
 
-	/*
-	Executes bound functions for released keys
-	*/
-	void onKeyRelease(int key);
+		/*
+		Adds a binding between a given functionand the key pressed. When pressed, GLFW will register the event and when polled will
+		call the bound function
+		*/
+		void addKeyPressBinding(Window* window, int key, const Callback callback);
 
-	/*
-	Executes events for held down keys
-	*/
-	void onKeyHold(int key);
+		/*
+		Adds a binding between a given functionand the key released. When released, GLFW will register the event and when polled will
+		call the bound function
+		*/
+		void addKeyReleaseBinding(Window* window, int key, const Callback callback);
 
-	/*
-	Determines whether the key event is for a pressed, released or held key and calls
-	the associated function
-	*/
-	void keyEvent(int key, int action);
+		/*
+		Adds a binding between a given functionand the key held. When held, GLFW will register the event and when polled will
+		call the bound function
+		*/
+		void addKeyHoldBinding(Window* window, int key, const Callback callback);
 
-	/*
-	Pointer to an instance of a GLFW Window
-	*/
-	GLFWwindow* m_instance;
+		/*
+		Removes any key press function binding on the given key
+		*/
+		void removeKeyPressBinding(Window* window, int key);
 
-private:
-	/*
-	Map of all bound key press functions and associated keys
-	*/
-    std::map<int, Callback> m_KeyPressCallbacks;
+		/*
+		Removes any key release function binding on the given key
+		*/
+		void removeKeyReleaseBinding(Window* window, int key);
 
-	/*
-	Map of all bound key release functions and associated keys
-	*/
-	std::map<int, Callback> m_KeyReleaseCallbacks;
+		/*
+		Removes any key hold function binding on the given key
+		*/
+		void removeKeyHoldBinding(Window* window, int key);
 
-	/*
-	Map of all bound key hold functions and associated keys
-	*/
-	std::map<int, Callback> m_KeyHoldCallbacks;
+		/*
+		Swaps the key associated with a bound key press function
+		*/
+		void swapKeyPressBinding(Window* window, int formerKey, int newKey);
 
-	/*
-	Window width, default value set to 800 pixels
-	*/
-	unsigned int m_SCREEN_WIDTH = 800;
+		/*
+		Swaps the key associated with a bound key release function
+		*/
+		void swapKeyReleaseBinding(Window* window, int formerKey, int newKey);
 
-	/*
-	Window height, default value set to 600 pixels
-	*/
-	unsigned int m_SCREEN_HEIGHT = 600;
+		/*
+		Swaps the key associated with a bound key hold function
+		*/
+		void swapKeyHoldBinding(Window* window, int formerKey, int newKey);
 
-	/*
-	Boolean value representing Fullscreen or windowed. If TRUE, application will run windowed
-	with the dimensions defined by m_SCREEN_WIDTH and m_SCREEN_HEIGHT. If FALSE, will 
-	run full screen with monitor dimensions
-	*/
-	bool m_windowedMode = true;
+		/*
+		Executes bound functions for pressed keys
+		*/
+		void onKeyPress(Window* window, int key);
 
-	/*
-	Function to be called on Window resize
-	*/
-	static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+		/*
+		Executes bound functions for released keys
+		*/
+		void onKeyRelease(Window* window, int key);
 
-	/*
-	Function to be called on key event
-	*/
-	static void keyEventCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-};
+		/*
+		Executes events for held down keys
+		*/
+		void onKeyHold(Window* window, int key);
+
+		/*
+		Determines whether the key event is for a pressed, released or held key and calls
+		the associated function
+		*/
+		void keyEvent(Window* window, int key, int action);
+	}
+}
 
 #endif // !WINDOW_H
