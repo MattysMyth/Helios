af360de1d6696bfa7c79d81b2ec3c9f1cf45e31d
MattysMyth <matthew.smith2193@gmail.com>
2023-01-08 22:14:37 +1100

    Update Window.cpp
    Split the Input functions to be under the namespace Input and the Window functions and variables to be under the Window class. Both are included in the Helios namespace

--- Engine/Window.cpp
+++ Engine/Window.cpp
@@ -1,6 +1,6 @@
 #include "Window.h"
 
-Window::Window()
+Helios::Window::Window()
 {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -17,121 +17,121 @@ Window::Window()
     }
 
     // Initialise a window, throw an error if window failed to initialise
-    m_instance = glfwCreateWindow(m_SCREEN_WIDTH, m_SCREEN_HEIGHT, "Window Title", monitorWindow, NULL);
-    if (m_instance == NULL)
+    m_GLFWinstance = glfwCreateWindow(m_SCREEN_WIDTH, m_SCREEN_HEIGHT, "Window Title", monitorWindow, NULL);
+    if (m_GLFWinstance == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
     }
-    glfwMakeContextCurrent(m_instance);
+    glfwMakeContextCurrent(m_GLFWinstance);
 
-    glfwSetWindowUserPointer(m_instance, this);
+    glfwSetWindowUserPointer(m_GLFWinstance, this);
     
-    glfwSetFramebufferSizeCallback(m_instance, framebuffer_size_callback);
-    glfwSetKeyCallback(m_instance, keyEventCallback);
+    glfwSetFramebufferSizeCallback(m_GLFWinstance, framebuffer_size_callback);
+    glfwSetKeyCallback(m_GLFWinstance, keyEventCallback);
 
-    addKeyPressBinding(GLFW_KEY_ESCAPE, [this]() {this->defaultEscape(); });
+    Helios::Input::addKeyPressBinding(this, GLFW_KEY_ESCAPE, [this]() {Helios::Input::defaultEscape(this); });
 }
 
-Window::~Window()
+Helios::Window::~Window()
 {
     
 }
 
-void Window::defaultEscape()
-{
-    glfwSetWindowShouldClose(m_instance, true);
-}
-
-void Window::framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void Helios::Window::framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
 
-void Window::keyEventCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+void Helios::Window::keyEventCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     Window* instance = static_cast<Window*>(glfwGetWindowUserPointer(window));
-    instance->keyEvent(key, action);
+    Helios::Input::keyEvent(instance, key, action);
 }
 
-void Window::keyEvent(int key, int action)
+void Helios::Input::defaultEscape(Window* window)
+{
+    glfwSetWindowShouldClose(window->m_GLFWinstance, true);
+}
+
+void Helios::Input::keyEvent(Window* window, int key, int action)
 {
     if (action == GLFW_PRESS)
     {
-        onKeyPress(key);
+        onKeyPress(window, key);
     }
     else if (action == GLFW_RELEASE)
     {
-        onKeyRelease(key);
+        onKeyRelease(window, key);
     }
     else if (action == GLFW_REPEAT)
     {
-        onKeyHold(key);
+        onKeyHold(window, key);
     }
 }
 
-void Window::addKeyPressBinding(int key, const Callback callback)
+void Helios::Input::addKeyPressBinding(Window* window, int key, const Callback callback)
 {
-    m_KeyPressCallbacks.insert({ key, callback });
+    window->m_KeyPressCallbacks.insert({ key, callback });
 }
 
-void Window::addKeyReleaseBinding(int key, const Callback callback)
+void Helios::Input::addKeyReleaseBinding(Window* window, int key, const Callback callback)
 {
-    m_KeyReleaseCallbacks.insert({key, callback});
+    window->m_KeyReleaseCallbacks.insert({key, callback});
 }
 
-void Window::addKeyHoldBinding(int key, const Callback callback)
+void Helios::Input::addKeyHoldBinding(Window* window, int key, const Callback callback)
 {
-    m_KeyHoldCallbacks.insert({ key, callback });
+    window->m_KeyHoldCallbacks.insert({ key, callback });
 }
 
-void Window::removeKeyPressBinding(int key)
+void Helios::Input::removeKeyPressBinding(Window* window, int key)
 {
-    m_KeyPressCallbacks.erase(key);
+    window->m_KeyPressCallbacks.erase(key);
 }
 
-void Window::removeKeyReleaseBinding(int key)
+void Helios::Input::removeKeyReleaseBinding(Window* window, int key)
 {
-    m_KeyReleaseCallbacks.erase(key);
+    window->m_KeyReleaseCallbacks.erase(key);
 }
 
-void Window::removeKeyHoldBinding(int key)
+void Helios::Input::removeKeyHoldBinding(Window* window, int key)
 {
-    m_KeyHoldCallbacks.erase(key);
+    window->m_KeyHoldCallbacks.erase(key);
 }
 
-void Window::swapKeyPressBinding(int formerKey, int newKey)
+void Helios::Input::swapKeyPressBinding(Window* window, int formerKey, int newKey)
 {
-    Callback callback = m_KeyPressCallbacks.at(formerKey);
-    removeKeyPressBinding(formerKey);
-    addKeyPressBinding(newKey, callback);
+    Callback callback = window->m_KeyPressCallbacks.at(formerKey);
+    Helios::Input::removeKeyPressBinding(window, formerKey);
+    Helios::Input::addKeyPressBinding(window, newKey, callback);
 }
 
-void Window::swapKeyReleaseBinding(int formerKey, int newKey)
+void Helios::Input::swapKeyReleaseBinding(Window* window, int formerKey, int newKey)
 {
-    Callback callback = m_KeyReleaseCallbacks.at(formerKey);
-    removeKeyReleaseBinding(formerKey);
-    addKeyReleaseBinding(newKey, callback);
+    Callback callback = window->m_KeyReleaseCallbacks.at(formerKey);
+    Helios::Input::removeKeyReleaseBinding(window, formerKey);
+    Helios::Input::addKeyReleaseBinding(window, newKey, callback);
 }
 
-void Window::swapKeyHoldBinding(int formerKey, int newKey)
+void Helios::Input::swapKeyHoldBinding(Window* window, int formerKey, int newKey)
 {
-    Callback callback = m_KeyHoldCallbacks.at(formerKey);
-    removeKeyHoldBinding(formerKey);
-    addKeyHoldBinding(newKey, callback);
+    Callback callback = window->m_KeyHoldCallbacks.at(formerKey);
+    Helios::Input::removeKeyHoldBinding(window, formerKey);
+    Helios::Input::addKeyHoldBinding(window, newKey, callback);
 }
 
-void Window::onKeyPress(int key)
+void Helios::Input::onKeyPress(Window* window, int key)
 {
-    std::invoke(m_KeyPressCallbacks[key]);
+    std::invoke(window->m_KeyPressCallbacks[key]);
 }
 
-void Window::onKeyRelease(int key)
+void Helios::Input::onKeyRelease(Window* window, int key)
 {
-    std::invoke(m_KeyReleaseCallbacks[key]);
+    std::invoke(window->m_KeyReleaseCallbacks[key]);
 }
 
-void Window::onKeyHold(int key)
+void Helios::Input::onKeyHold(Window* window, int key)
 {
-    std::invoke(m_KeyHoldCallbacks[key]);
+    std::invoke(window->m_KeyHoldCallbacks[key]);
 }
